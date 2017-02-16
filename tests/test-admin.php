7fa1f9ff30443492e9f80862be6fba575c5a2e51
Josh Harris <josh.harris@k2sports.com>
2017-02-16 11:39:31 -0800

    setting up test (wip)

--- tests/test-admin.php
+++ tests/test-admin.php
@@ -6,10 +6,25 @@
  */
 
 class AdminTest extends WP_UnitTestCase {
-	private $plugin;
+	private $plugin,
+		$fields = array(
+			'broes_api_key' => '',
+			'broes_webform_url' => '',
+			'broes_webform_secret' => '',
+			'broes_contact' => '',
+			'broes_list_ids' => '',
+			'broes_fields' => '',
+			'broes_required_fields' => '',
+			'broes_cta' => '',
+			'broes_success_message' => '',
+			'broes_registered_message' => ''
+		);
 
 	public function setUp() {
 		parent::setUp();
+		foreach ($this->fields as $key => $value) {
+			add_option($key, $value);
+		}
 	}
 
 	public function tearDown() {
@@ -19,5 +34,12 @@ class AdminTest extends WP_UnitTestCase {
 	public function testPluginActive() {
 		$test = is_plugin_active( 'bronto-email-signup/bronto-email-signup.php' );
 		$this->assertTrue( $test );
+	}
+
+	public function testAdminInit() {
+		$this->setUp();
+		foreach ( $this->fields as $key => $value ) {
+			$this->assertTrue( empty( get_option( $key ) ) );
+		}
 	}
 }
