64dae3927903facda440cec61d6bf1c2b062fcf7
Josh Harris <josh.harris@k2sports.com>
2017-03-03 09:40:32 -0800

    update setup() with bootstrap for plugin

--- tests/test-admin.php
+++ tests/test-admin.php
@@ -23,6 +23,9 @@ class AdminTest extends WP_UnitTestCase {
 	public function setUp() {
 		parent::setUp();
 		activate_bronto_email_signup();
+		$this->plugin = new Bronto_Email_Signup();
+		$this->plugin_admin = new Bronto_Email_Signup_Admin( $this->plugin->get_bronto_email_signup(), $this->plugin->get_version(), $this->plugin->get_option_fields() );
+		$this->fields = $this->plugin->get_option_fields();
 	}
 
 	public function testPluginActive() {
